0926cd7b36b7c4acc867d87ce76e534bfa9278d4
nas97 <nas9@tpu.ru>
2025-09-13 15:22:19 +0700

    test5

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include "version.h"
 
 int main() {
-    std::cout << "Hello, World! Version " << VERSION_MAJOR << '.' << VERSION_MINOR << '.' << VERSION_PATCH << std::endl;
+    std::cout << "Hello world! Version " << VERSION_MAJOR << '.' << VERSION_MINOR << '.' << VERSION_PATCH << std::endl;
     return 0;
 }
