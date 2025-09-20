82f146b3a714c39bebc10486c614fde3dff00c69
nikitasemenchenko <semenchenko_06@mail.ru>
2025-09-20 10:30:32 +0700

    test7

--- functions.h
+++ functions.h
@@ -10,5 +10,5 @@ void mainSort(std::vector<std::tuple<int, int, int, int>>& start_vector);
 std::vector<std::tuple<int, int, int, int>> parse(const std::string& file_name);
 void printFullList(std::vector<std::tuple<int, int, int, int>>& list_of_ip);
 void printWith1(std::vector<std::tuple<int, int, int, int>>& list_of_ip);
-void printWith46And70(std::vector<std::tuple<int, int, int, int>>& list_of_ip);
 void printWith46(std::vector<std::tuple<int, int, int, int>>& list_of_ip);
+void printWith46And70(std::vector<std::tuple<int, int, int, int>>& list_of_ip);
