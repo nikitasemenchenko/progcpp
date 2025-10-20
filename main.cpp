3c1274c3c4b50d189c3406ad147cee7a2f893b94
nikitasemenchenko <semenchenko_06@mail.ru>
2025-10-21 01:34:29 +0700

    second test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <map>
 #include "myAllocator.h"   // ← убедись, что имя файла совпадает!
-#include "MyVector.h"
+#include "myVector.h"
 
 int factorial(int n) {
     if (n == 0) return 1;
